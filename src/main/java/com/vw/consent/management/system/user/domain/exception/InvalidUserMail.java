534f34993dc938a6019d0071dbb68ac8231a2a2a
carlos-araujo-ingka <carlos.araujo3@ingka.ikea.com>
2025-08-08 17:13:21 +0200

    Changes

---
+++ src/main/java/com/vw/consent/management/system/user/domain/exception/InvalidUserMail.java
@@ -0,0 +1,7 @@
+package com.vw.consent.management.system.user.domain.exception;
+
+public class InvalidUserMail extends RuntimeException {
+    public InvalidUserMail(String message) {
+        super(message);
+    }
+}
